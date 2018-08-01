8dda074bb5fd4094468d21e9d0bc5a9c415ce70a
Simon Coope <simon.coope@bjss.com>
2018-08-01 11:55:46 +0100

    Fixed some hardcoding issues

--- src/queries/getAllVehicles.ts
+++ src/queries/getAllVehicles.ts
@@ -1,24 +1,97 @@
 import gql from "graphql-tag";
 
-export const listAllVehicles = gql`
-        query GetVehicles {
-            getVehicles {
-                vin,
-                timeStamp,
-                vehicle {
-                    systemNumber
-                    vehicleClass
-                    vehicleType
-                    chassisType
-                    vin
-                    manufactureDate
-                    testCertificateExpirtyDate
-                    vehicleIdentifier
-                    previousRegmark
-                    registrationDate
-                    numberOfAxles
-                    make
-                    model
+export const query = gql`
+query GetVehicles {
+    getVehicles {
+        vin,
+        timeStamp,
+        vehicle {
+            systemNumber
+            vehicleClass
+            vehicleType
+            chassisType
+            vin
+            manufactureDate
+            testCertificateExpirtyDate
+            vehicleIdentifier
+            previousRegmark
+            registrationDate
+            numberOfAxles
+            make
+            model
+        }, 
+        techRecord {
+            bodyModel,
+            bodyMake,
+            bodyType,
+            chassisMake,
+            chassisModel,
+            brakeCode,
+            grossDesignWeight,
+            grossGbWeight,
+            grossKerbWeight,
+            grossLadenWeight,
+            retarderBrake1,
+            retarderBrake2,
+            seatsLowerDeck,
+            seatsUpperDesk,
+            sequenceNumber,
+            speedLimiterMarker,
+            speedRestriction,
+            standingCapacity,
+            tachoExemptMarker,
+            unladenWeight,
+            dispensations,
+            remarks,
+            serviceA,
+            secA,
+            parkingA,
+            serviceB,
+            secB,
+            parkingB,
+            brakeData {
+                secondary,
+                parking,
+                brakeDataCode,
+                service
+            },
+            axles {
+                axleNumber,
+                weights {
+                gbWeight,
+                designWeight
+                },
+                tyres {
+                tyreCode,
+                tyreSize,
+                plyRating,
+                fitmentCode,
+                dLoadIndex,
+                sLoadIndex,
+                loadIndex,
+                speedCatSymbol
                 }
             }
-        }`;
+        },
+        operator {
+            operatorLicenceNumber,
+            operatorLicenceStatus,
+            operatorName
+        },
+        testHistory {
+            location,
+            testDate,
+            testType,
+            vehicleIdentifierAtTest,
+            testResult,
+            testCertificateExpiryDateAtTest,
+            numberOfDefectsAtTest,
+            numberOfAdvisoryDefectsAtTest
+        },
+        encounter {
+            prohibitionNotice,
+            prohibitionOutstanding
+            }
+        }
+    }
+`;
